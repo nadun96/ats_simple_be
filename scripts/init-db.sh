c13e69b768b7b71080972d0186c549eb7447abdd
nadun96 <naadunudaraka@gmail.com>
2025-08-14 17:16:59 +0530

    init db script changed

--- scripts/init-db.sh
+++ scripts/init-db.sh
@@ -1,31 +1,41 @@
 #!/bin/bash
 set -e
 
+echo "Creating databases for ATS services..."
+
+# Create databases for each service
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    -- Create databases for each service
     CREATE DATABASE auth_db;
     CREATE DATABASE ats_db;
     CREATE DATABASE email_db;
     CREATE DATABASE notification_db;
-    
-    -- Grant all privileges to the user for all databases
-    GRANT ALL PRIVILEGES ON DATABASE auth_db TO $POSTGRES_USER;
-    GRANT ALL PRIVILEGES ON DATABASE ats_db TO $POSTGRES_USER;
-    GRANT ALL PRIVILEGES ON DATABASE email_db TO $POSTGRES_USER;
-    GRANT ALL PRIVILEGES ON DATABASE notification_db TO $POSTGRES_USER;
-    
-    -- Connect to each database and grant schema privileges
-    \c auth_db;
-    GRANT ALL ON SCHEMA public TO $POSTGRES_USER;
-    
-    \c ats_db;
-    GRANT ALL ON SCHEMA public TO $POSTGRES_USER;
-    
-    \c email_db;
-    GRANT ALL ON SCHEMA public TO $POSTGRES_USER;
-    
-    \c notification_db;
-    GRANT ALL ON SCHEMA public TO $POSTGRES_USER;
 EOSQL
 
-echo "All databases created successfully!"
+echo "Granting privileges to user $POSTGRES_USER..."
+
+# Grant privileges for each database
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "auth_db" <<-EOSQL
+    GRANT ALL ON SCHEMA public TO $POSTGRES_USER;
+    GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO $POSTGRES_USER;
+    GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO $POSTGRES_USER;
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "ats_db" <<-EOSQL
+    GRANT ALL ON SCHEMA public TO $POSTGRES_USER;
+    GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO $POSTGRES_USER;
+    GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO $POSTGRES_USER;
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "email_db" <<-EOSQL
+    GRANT ALL ON SCHEMA public TO $POSTGRES_USER;
+    GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO $POSTGRES_USER;
+    GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO $POSTGRES_USER;
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "notification_db" <<-EOSQL
+    GRANT ALL ON SCHEMA public TO $POSTGRES_USER;
+    GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO $POSTGRES_USER;
+    GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO $POSTGRES_USER;
+EOSQL
+
+echo "All databases created and privileges granted successfully!"
